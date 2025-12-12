6c7223e8922fcdf31342d0c44fb25545e34d0b51
HoaiVu <lehoaivu.10092005@gmaill.com>
2025-12-12 23:00:32 +0700

    chuan bi deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,27 @@ import router from "./routes/taskRoutes.js";
 import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5003;
+const __dirname = path.resolve();
+
 connectDb();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
-
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use("/api/tasks", router);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen("5003", () => {
   console.log(`Server đang chạy trên cổng ${PORT}`);
